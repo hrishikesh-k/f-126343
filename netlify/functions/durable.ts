7747d13313f6559fa0bb22ef3a35bf3388511b1c
Hrishikesh Kokate <hrishi5200@gmail.com>
2024-09-29 14:22:59 +0000

    add vary

--- netlify/functions/durable.ts
+++ netlify/functions/durable.ts
@@ -1,18 +1,18 @@
 import type {Config} from '@netlify/functions'
 
 export default async function(req : Request) {
+  const headers = new Headers({
+    'netlify-cdn-cache-control': 'durable, max-age=3600',
+    'netlify-vary': 'query'
+  })
   const url = new URL(req.url)
   if (url.pathname.startsWith('/page-1/')) {
     return new Response('page-1', {
-      headers: {
-        'netlify-cdn-cache-control': 'durable, max-age=3600'
-      }
+      headers
     })
   }
   return new Response('home', {
-    headers: {
-      'netlify-cdn-cache-control': 'durable, max-age=3600'
-    }
+    headers
   })
 }
 
